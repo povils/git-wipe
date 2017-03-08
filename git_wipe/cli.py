44576c49cfc495c7e8e2f0758a4c712a1cbaf3c7
Povilas Susinskas <povilas.susinskas@gmail.com>
2017-03-08 23:36:10 +0000

    add new tests

--- git_wipe/cli.py
+++ git_wipe/cli.py
@@ -30,22 +30,17 @@ def cleanup(token, timeout, skip_repository, skip_branch, preview, no_interactio
     if token is None:
         token = click.prompt(crayons.green('Please enter your Github access token'))
 
-    print(list(skip_repository))
-    print(skip_branch)
     github_client = GithubClient(token, timeout)
     try:
         click.echo(crayons.green('Searching for branches. This may take a while...'), err=True)
         with blindspin.spinner():
-            #repo_branches = github_client.get_merged_fork_branches(skip_repository, skip_branch)
-            repo_branches = github_client.get_merged_fork_branches(skip_repository, 'b')
+            repo_branches = github_client.get_merged_fork_branches(skip_repository, skip_branch)
     except BadCredentialsException:
         click.echo(crayons.red('Bad credentials. Please provide valid access token'), err=True)
         sys.exit(1)
-
     if not repo_branches:
         click.echo(crayons.green('Congratulations! No remote branches are available for cleaning up'))
         sys.exit(0)
-
     list_branches(repo_branches)
 
     if False == preview:
--- git_wipe/tests/test_cli.py
+++ git_wipe/tests/test_cli.py
@@ -7,6 +7,8 @@ from click.testing import CliRunner
 from git_wipe.cli import (cli, cleanup)
 from git_wipe.__version__ import __version__
 from github.GithubException import BadCredentialsException
+from github.Repository import Repository
+from github.Branch import Branch
 from git_wipe.github_client import GithubClient
 
 class TestCli():
@@ -27,7 +29,9 @@ class TestCli():
         assert result.output == 'git-wipe, version ' + __version__ + '\n'
 
 
-    def test_cli_cleanup_wihout_any_options(self):
+    @patch.object(GithubClient, 'get_merged_fork_branches')
+    def test_cli_cleanup_wihout_any_options(self, get_merged_fork_branches):
+        get_merged_fork_branches.side_effect = []
         runner = CliRunner()
         result = runner.invoke(cleanup, input = 'token')
 
@@ -39,7 +43,7 @@ class TestCli():
         runner = CliRunner()
         result = runner.invoke(cleanup, ['--token=token'])
 
-       # get_merged_fork_branches.assert_called_with((), ())
+        get_merged_fork_branches.assert_called_with((), ())
         assert result.exit_code == 1
         assert 'Bad credentials' in result.output
 
@@ -50,13 +54,60 @@ class TestCli():
         options = [
             '--token=token',
             '--skip-repository=repository_1',
-            '--skip-repository=repostiory_2',
+            '--skip-repository=repository_2',
             '--skip-branch=branch_1',
             '--skip-branch=branch_2',
         ]
         result = runner.invoke(cleanup, options)
-        print(result.output)
-        #get_merged_fork_branches.assert_called_with(('repository_1', 'repository_2'), ('branch_1', 'branch_2'))
-        get_merged_fork_branches.assert_called_with(('repository_1', 'repository_2'), 'b')
+        get_merged_fork_branches.assert_called_with(('repository_1', 'repository_2'), ('branch_1', 'branch_2'))
+        assert 'Congratulations' in result.output
+    
+    @patch('github.Repository.Repository')
+    @patch('github.Branch.Branch')
+    @patch.object(GithubClient, 'get_merged_fork_branches')
+    @patch.object(GithubClient, 'delete_branches')
+    def test_cli_cleanup_list_find_branches_and_ask_to_delete(self, delete_branches, get_merged_fork_branches, mocked_branch, mocked_repository):       
+        mocked_repository.full_name = 'repo_name'
+        mocked_branch.name = 'branch_name'
+        repo_branches = [
+            [mocked_repository, mocked_branch]
+        ]
+        
+        get_merged_fork_branches.return_value = repo_branches
+ 
+        runner = CliRunner()
+        options = [
+            '--token=token',
+        ]
+        
+        result = runner.invoke(cleanup, options, input = 'y')
+        delete_branches.assert_called_with(repo_branches)
+        assert 'Searching' in result.output
+        assert 'repo_name:branch_name' in result.output
+        assert 'Delete these branches?' in result.output
+        assert 'Deleting branches' in result.output
+        assert 'Done' in result.output
 
+    @patch('github.Repository.Repository')
+    @patch('github.Branch.Branch')
+    @patch.object(GithubClient, 'get_merged_fork_branches')
+    @patch.object(GithubClient, 'delete_branches')
+    def test_cli_cleanup_with_preview_option_should_not_ask_to_delete(self, delete_branches, get_merged_fork_branches, mocked_branch, mocked_repository):
+        mocked_repository.full_name = 'repo_name'
+        mocked_branch.name = 'branch_name'
+        repo_branches = [
+            [mocked_repository, mocked_branch]
+        ]
+        get_merged_fork_branches.return_value = repo_branches
 
+        runner = CliRunner()
+        options = [
+            '--token=token',
+            '--preview',
+        ]
+
+        result = runner.invoke(cleanup, options)
+        
+        assert 'Delete these branches?' not in result.output
+        assert 'Deleting branches' not in result.output
+        assert not delete_branches.called
