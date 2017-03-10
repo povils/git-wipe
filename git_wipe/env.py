b3115d31ef18dce434833dc1fa4445f2ef65915c
Povilas Susinskas <povilas.susinskas@gear4music.com>
2017-03-10 15:47:05 +0000

    remove timeout env

--- git_wipe/env.py
+++ git_wipe/env.py
@@ -3,9 +3,3 @@ import os
 # Github access token used for git-wipe cli tool
 GIT_WIPE_TOKEN = os.environ.get('GIT_WIPE_TOKEN')
 
-# Github requester timeout in seconds
-GIT_WIPE_TIMEOUT = os.environ.get('GIT_WIPE_TIMEOUT')
-if GIT_WIPE_TIMEOUT is None:
-    GIT_WIPE_TIMEOUT = 30
-else:
-    GIT_WIPE_TIMEOUT = int(GIT_WIPE_TOKEN)
